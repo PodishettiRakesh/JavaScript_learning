3957ca36e29d1e9e856f3ea4c9a2b4a3521ca841
PodishettiRakesh <podishettirakesh70@gmail.com>
2024-05-08 10:04:58 +0530

    JavaScript code_blocks

---
+++ learn.js
@@ -0,0 +1,12 @@
+// let a, b, c;
+a = 2; b = 3; c = a + b+b;
+
+console.log("The value of c is " + c + ".");
+
+
+function demo(){
+    console.log("how can we print")
+    console.log("\n")
+    console.log("yes we print that")
+}
+demo()
