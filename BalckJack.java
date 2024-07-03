d40e33f5d5aa7bf462f7882f9e1c154e0f664b82
PodishettiRakesh <podishettirakesh70@gmail.com>
2024-07-03 10:40:44 +0530

    intialized SUITS and RANKS as final

---
+++ BalckJack.java
@@ -0,0 +1,6 @@
+class BlackJava{
+    private static final char[] SUITS = {'H','D','C','S'};
+    private static final String[] RANKS = {"2","3","4","5","6","7","8","9","10","J","Q","K","A"};
+
+
+}
