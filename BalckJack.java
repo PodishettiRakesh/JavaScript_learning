23e6498419191eeaaa86e186f8ceba7cd11efb6b
PodishettiRakesh <podishettirakesh70@gmail.com>
2024-07-03 10:53:35 +0530

    imported required ArrayList and LI=ist inbuilt methods

--- BalckJack.java
+++ BalckJack.java
@@ -1,6 +1,14 @@
+import java.util.ArrayList;
+import java.util.List;
 class BlackJava{
     private static final char[] SUITS = {'H','D','C','S'};
     private static final String[] RANKS = {"2","3","4","5","6","7","8","9","10","J","Q","K","A"};
+    private static final int MAX_POINTS =21;
+    private static final int DEALER_MIN_POINTS =17;
+
+    private List<String> deck;
+    private List<String> playerHand;
+    private List<String> dealerHand;
 
 
 }
