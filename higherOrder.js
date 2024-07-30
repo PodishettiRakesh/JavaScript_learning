be412aa117e991f2d422769cae26d9ab6b02160a
PodishettiRakesh <podishettirakesh70@gmail.com>
2024-07-30 10:35:52 +0530

    The forEach method adds each element to the sum variable.

--- higherOrder.js
+++ higherOrder.js
@@ -2,8 +2,14 @@
 
 
 //  Simple iteration with single parameter
-const numbers=[1,2,3,4,5];
+const numbers=[1,2,3,4,5,6,7,8,9];
 
+// numbers.forEach(number =>{
+//     console.log(number);
+// })
+
+let sum=0;
 numbers.forEach(number =>{
-    console.log(number);
+    sum+=number;
 })
+console.log(sum);
