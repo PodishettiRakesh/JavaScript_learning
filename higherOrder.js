3108949bf6b249247c128f852bcf2d3ad8881793
PodishettiRakesh <podishettirakesh70@gmail.com>
2024-07-30 10:40:37 +0530

    The forEach method iterates over each element, printing the element and its index.

--- higherOrder.js
+++ higherOrder.js
@@ -8,8 +8,14 @@ const numbers=[1,2,3,4,5,6,7,8,9];
 //     console.log(number);
 // })
 
-let sum=0;
-numbers.forEach(number =>{
-    sum+=number;
+// let sum=0;
+// numbers.forEach(number =>{
+//     sum+=number;
+// })
+// console.log(sum);
+
+numbers.forEach((num, ind,arra,array)=>{
+    console.log(`the element at index ${ind} is ${num}`);
+    console.log(arra);
+    console.log(array);
 })
-console.log(sum);
