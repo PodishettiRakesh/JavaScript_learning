e1aa6a716923549de9fa326915fddb92e3ad9cb7
PodishettiRakesh <podishettirakesh70@gmail.com>
2024-07-30 10:49:43 +0530

    Iteration with all three parameters (element, index, array)

--- higherOrder.js
+++ higherOrder.js
@@ -20,14 +20,17 @@ const numbers=[1,2,3,4,5,6,7,8,9];
 //     console.log(array);
 // })
 
-let evenCount=0;
-let oddCount=0;
+// let evenCount=0;
+// let oddCount=0;
+// numbers.forEach(number=>{
+//     if(number % 2==0){
+//         evenCount++;
+//     }else{
+//         oddCount++;
+//     }
+// })
+// console.log(`even number count${evenCount} and odd count ${oddCount}`);
 
-numbers.forEach(number=>{
-    if(number % 2==0){
-        evenCount++;
-    }else{
-        oddCount++;
-    }
+numbers.forEach((num, ind, array)=>{
+    console.log(`${num} at positin ${ind}   Array: ${[array.join(" @ ")]}`);
 })
-console.log(`even number count${evenCount} and odd count ${oddCount}`);
