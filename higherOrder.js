14eae3db4700a3d8bed54fe9e94eb0c4fd7b11d2
PodishettiRakesh <podishettirakesh70@gmail.com>
2024-07-30 10:44:26 +0530

    The forEach method counts how many numbers are even and how many are odd.

--- higherOrder.js
+++ higherOrder.js
@@ -14,8 +14,20 @@ const numbers=[1,2,3,4,5,6,7,8,9];
 // })
 // console.log(sum);
 
-numbers.forEach((num, ind,arra,array)=>{
-    console.log(`the element at index ${ind} is ${num}`);
-    console.log(arra);
-    console.log(array);
+// numbers.forEach((num, ind,arra,array)=>{
+//     console.log(`the element at index ${ind} is ${num}`);
+//     console.log(arra);
+//     console.log(array);
+// })
+
+let evenCount=0;
+let oddCount=0;
+
+numbers.forEach(number=>{
+    if(number % 2==0){
+        evenCount++;
+    }else{
+        oddCount++;
+    }
 })
+console.log(`even number count${evenCount} and odd count ${oddCount}`);
